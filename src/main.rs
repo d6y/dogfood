fe29f867a363358c5d7451d2ee6d4fa1a9cb16a6
Richard Dallaway <richard@dallaway.com>
2020-09-01 18:02:07 +0100

    add command line args

--- src/main.rs
+++ src/main.rs
@@ -4,54 +4,68 @@ use take_until::TakeUntilExt;
 mod diagram;
 mod svg;
 use diagram::Diagram;
+use structopt::StructOpt;
 
 type Fraction = Ratio<u16>;
 type Gram = u16;
 
+#[derive(Debug, StructOpt)]
+struct Args {
+    /// How much food is in a can, in grams?
+    #[structopt(short, long, default_value = "400")]
+    can_contents: Gram,
+
+    /// How much does an empty can weigh, in grams?
+    #[structopt(short, long, default_value = "52")]
+    empty_can: Gram,
+
+    /// Filename for SVG output
+    #[structopt(short, long)]
+    svg: Option<String>,
+
+    numerator: u16,
+    denominator: u16,
+}
+
 fn main() {
-    let numer = 1;
-    let denom = 5;
+    let args = Args::from_args();
+    assert!(args.denominator > 0); // avoid divide by zero
+    assert!(args.numerator > 0); // must take a reducing step
+    assert!(args.numerator <= args.denominator); // must be a fraction of the whole
 
-    assert!(denom > 0); // avoid divide by zero
-    assert!(numer > 0); // must take a reducing step
-    assert!(numer <= denom); // must be a fraction of the whole
-
-    let can_contents: Gram = 400; // how much food in a can?
-    let empty_can: Gram = 52; // what does an empty can weigh?
-
-    let reduction = Fraction::new(numer, denom);
+    let reduction = Fraction::new(args.numerator, args.denominator);
 
     let day_fractions = dogfood(reduction);
 
     println!("Starting with a full can...");
-    for day in &day_fractions {
-        println!("{}", day);
+    for (i, day) in day_fractions.iter().enumerate() {
+        println!("End of day {}: {} left over", 1 + i, day);
     }
 
-    let labeller = weight_labeller(can_contents, empty_can);
-    let weight_labels = day_fractions.iter().map(labeller);
+    if let Some(filename) = args.svg {
+        let labeller = weight_labeller(args.can_contents, args.empty_can);
+        let weight_labels = day_fractions.iter().map(labeller);
 
-    let row = |weight| -> Diagram { Diagram::can().above(Diagram::label(weight)) };
+        let row = |weight| -> Diagram { Diagram::can().above(Diagram::label(weight)) };
 
-    let diagram = &weight_labels.fold(Diagram::new(), |diagram, weight| diagram.above(row(weight)));
+        let diagram =
+            &weight_labels.fold(Diagram::new(), |diagram, weight| diagram.above(row(weight)));
 
-    dbg!(diagram);
+        dbg!(diagram);
 
-    svg::save(&diagram, "image.svg")
+        svg::save(&diagram, &filename)
+    }
 }
 
 fn dogfood(reduction: Fraction) -> Vec<Fraction> {
     let full_can = Fraction::new(1, 1);
-    let empty_can = Fraction::new(0, 1);
-
     let is_empty = |can: &Fraction| can.numer() == &0;
 
     let step = |start: &Fraction| -> Fraction {
-        let remains = start - reduction;
-        if remains >= empty_can {
-            remains
+        if *start >= reduction {
+            start - reduction
         } else {
-            full_can + remains
+            full_can + start - reduction
         }
     };
 
@@ -63,6 +77,22 @@ fn dogfood(reduction: Fraction) -> Vec<Fraction> {
     .take_until(is_empty)
     .take(100) // safety net on how long this runs for
     .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_two_thirds() {
+        assert_eq!(
+            dogfood(Fraction::new(2, 3)),
+            vec![
+                Fraction::new(1, 3),
+                Fraction::new(2, 3),
+                Fraction::new(0, 3)
+            ]
+        );
+    }
 }
 
 fn weight_labeller(can_contents: Gram, empty_can: Gram) -> impl Fn(&Fraction) -> String {
